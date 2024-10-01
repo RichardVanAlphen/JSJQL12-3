228c99befbc32a08d5e555c4892ab3e5d395c546
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2024-10-01 13:38:24 +0200

    Update script12-3.js

--- script12-3.js
+++ script12-3.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#btnLoad').on('click',function(){
-        let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={977ebd2623bf8fc4cc87024f01e7c019}';
+        let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?appid={977ebd2623bf8fc4cc87024f01e7c019&q=}';
         weatherUrl += $('#txtCity').val();
         $.ajax({
             url: weatherUrl,
